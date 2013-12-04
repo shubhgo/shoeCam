006ad9219c26f98afc7dc3368c5096b830be162e
Shubham Goel <shubham.goel@gmail.com>
2013-12-04 01:16:37 -0800

    - activate on double tap - camera function called when activated

---
+++ ActivateNClick/ActivateNClick.ino
@@ -0,0 +1,133 @@
+const int activateButton = 2;
+const int cameraButton = 3;
+const int flashPin = 13;
+
+int activateButtonState = 0;
+int activateFirstTap = 0;
+unsigned long activateFirstTapTime = 0;
+
+boolean activeNow = false;
+unsigned long activatedAt = 0;
+float doubleTapTime = 2000;
+float autoDeactivateTime = 3000;
+
+void setup()
+{
+  pinMode(flashPin, OUTPUT);
+  pinMode(activateButton, INPUT);
+  Serial.begin(9600);
+  Serial.println("setup");
+}
+
+void loop()
+{
+  if (shouldActivate())
+  {
+    Serial.println("turn On");
+    shouldTurnOnFlashLight(true);
+    activatedAt = millis();
+    activeNow = true;
+  }
+  
+  if (shouldDeactivate())
+  {
+    Serial.println("auto Off");
+    shouldTurnOnFlashLight(false);
+    activatedAt = 0;
+    activeNow = false;
+  }
+  
+  if (activeNow)
+  {
+    int clickButtonPressed = digitalRead(cameraButton);
+    if (clickButtonPressed)
+    {
+      Serial.println("Click!");
+      activatedAt = 0;
+      activeNow = false;
+       tower
+      // 1 Flashlight off
+      shouldTurnOnFlashLight(false);
+      
+      // 2 Take a picture
+      takePicture();
+      // 3 Motor on for some time
+      // 4 get location
+      // 5 upload to net
+      
+    }
+  }
+}
+
+void shouldTurnOnFlashLight(boolean state)
+{
+  if(state == true)
+  {
+    digitalWrite(flashPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(flashPin, LOW);
+  }
+}
+
+
+boolean shouldDeactivate()
+{
+  if (activatedAt == 0)
+  {
+    return false;
+  }
+  
+  int currentTime = millis();
+  if ((currentTime - activatedAt) > autoDeactivateTime)
+  {
+    return true;
+  }else
+  {
+    return false;
+  }
+}
+
+
+boolean shouldActivate()
+{
+  activateButtonState = digitalRead(activateButton);
+  if (activateButtonState == HIGH)
+  {
+    delay(250);
+    // first press
+    if (activateFirstTap == 0)
+    {
+//      Serial.println("first tap");
+      activateFirstTap = 1;
+      activateFirstTapTime = millis();
+      return false;
+    }else
+    {
+      // second tap
+      int currentTime = millis();
+      if ((currentTime-activateFirstTapTime)<doubleTapTime)
+      {
+//        Serial.println("second tap");
+        // activate
+        activateFirstTap = 0;
+        activateFirstTapTime = 0;
+        return true;
+      }
+      else
+      {
+//        Serial.println("late first tap");
+        // deactivate and register first tap
+        activateFirstTap = 1;
+        activateFirstTapTime = millis();
+        return false;
+      }
+    }
+  }
+}
+
+void takePicture()
+{
+  Serial.println("takePicture: __incomplete__");
+}
