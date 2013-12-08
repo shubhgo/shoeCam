d62fcc8dc7259cfdc9ad232c9d65bae384bf33fb
100kristine <100kristine@users.noreply.github.com>
2013-12-08 00:47:35 -0800

    Update webCamCV.py

--- Camera/webCamCV.py
+++ Camera/webCamCV.py
@@ -3,13 +3,22 @@ import cv,os,random,serial
 #sudo chown -R `whoami` /opt/local is my friend
 
 ser = serial.Serial('/dev/tty.usbmodemfd121',9600)
+#or ser = serial.Serial('/dev/tty.usbmodemfa131',9600)
 
 def getSerialOutput(ser):
-	return ser.readline()
+	"""If Serial returns c, takes a photo and halts."""
+	while True: 
+		r = ser.read()
+		if r == "c":
+			takePhoto()
+		break
+	return 
 
 #
 
 def takePhoto():
+	"""Takes a photo with webcam and gives it a name with a random number. Saves photo to current
+	directory."""
 	photoName = "IMG_"+random.randint(1,900) + ".jpg" #idk if we want to change this later
 	
 	cv.NamedWindow("w1", cv.CV_WINDOW_AUTOSIZE)
